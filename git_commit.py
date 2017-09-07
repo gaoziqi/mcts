76fb46d841eb30e79e9ed4df41546cf19a722fd9
gaoziqi <571051761@qq.com>
2017-09-07 15:47:50 +0800

    2017-09-07 15:47:50

--- git_commit.py
+++ git_commit.py
@@ -8,18 +8,22 @@ import time
 import getopt
 import sys
 
-# tdq$abc123
 
 if __name__ == '__main__':
     commit = 1
+    force = False
     # 获取输入参数 -c commit
-    opts, args = getopt.getopt(sys.argv[1:], "hp", ['help', 'pull'])
+    opts, args = getopt.getopt(sys.argv[1:], "hpf", ['help', 'pull', 'force'])
     for op, value in opts:
         if op in ("-p", "--pull"):
             commit = 0
+        elif op in ("-f", "--force"):
+            commit = 0
+            force = True
         elif op in ("-h", "--help"):
             print('-h     : print this help message and exit (also --help)')
             print('-p     : git pull  (also --pull)')
+            print('-f     : git pull force  (also --force)')
             sys.exit()
     if commit:
         os.system('git add *')
@@ -28,6 +32,11 @@ if __name__ == '__main__':
         os.system('git push origin master')
         print('提交完成')
     else:
-        os.system('git fetch origin master')
-        os.system('git merge origin/master')
+        if force:
+            os.system('git fetch --all')
+            os.system('git reset --hard origin/master')
+            os.system('git pull')
+        else:
+            os.system('git fetch origin master')
+            os.system('git merge origin/master')
         print('更新完成')
